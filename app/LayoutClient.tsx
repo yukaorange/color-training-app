e580b257b4ea267d741732abd56f2d4526ca6b1a
yukaorange <takaoka.webdesigner@gmail.com>
2024-09-27 06:14:19 +0900

    change hover interaction in archive item

--- app/LayoutClient.tsx
+++ app/LayoutClient.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import '@/app/styles/layout.scss';
 import { ViewPortCalculator } from '@/components/Utility/ViewportCalculator';
 import { UserAgent } from '@/components/Utility/UserAgent';
 import { usePathname } from 'next/navigation';
@@ -7,9 +8,7 @@ import { Header } from '@/components/Layout/Header/Header';
 import { Measure } from '@/components/Layout/Measure/Measure';
 import { BgGrid } from '@/components/Layout/BgGrid/BgGrid';
 
-import '@/app/styles/layout.scss';
 import { useRef } from 'react';
-import { useLoadState } from '@/hooks/useLoadState';
 
 export const LayoutClient = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   const pathname = usePathname();
