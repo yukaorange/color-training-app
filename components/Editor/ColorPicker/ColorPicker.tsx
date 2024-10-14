d4c006852b37bf08c4a26c0fc1f008ff6906e122
yukaorange <takaoka.webdesigner@gmail.com>
2024-10-14 15:46:00 +0900

    add next.config.js

--- components/Editor/ColorPicker/ColorPicker.tsx
+++ components/Editor/ColorPicker/ColorPicker.tsx
@@ -1,6 +1,6 @@
 import '@/components/Editor/ColorPicker/styles/color-picker.scss';
-import '@/components/Editor/ColorPicker/styles/toggle-color-picker.scss';
 import '@/components/Editor/ColorPicker/styles/button-done.scss';
+import '@/components/Editor/ColorPicker/styles/toggle-color-picker.scss';
 import '@/components/Editor/ColorPicker/styles/toggle-element-picker.scss';
 
 import { hexToHsva } from '@uiw/color-convert';
