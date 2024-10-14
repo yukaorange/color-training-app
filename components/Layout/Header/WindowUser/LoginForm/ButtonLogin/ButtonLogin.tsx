986a527ad715903c3f20617803fdf6fe7436950b
yukaorange <takaoka.webdesigner@gmail.com>
2024-10-14 19:54:32 +0900

    change button google auth

--- components/Layout/Header/WindowUser/LoginForm/ButtonLogin/ButtonLogin.tsx
+++ components/Layout/Header/WindowUser/LoginForm/ButtonLogin/ButtonLogin.tsx
@@ -14,7 +14,8 @@ export const ButtonLogin = ({ onClick, text, variant }: ButtonLoginProps) => {
     >
       {variant === 'google' ? (
         <span>
-          Sign in with 
+          Sign in with
+          <span> </span>
           <span className="button-login__char--google">G</span>
           <span className="button-login__char--google">o</span>
           <span className="button-login__char--google">o</span>
