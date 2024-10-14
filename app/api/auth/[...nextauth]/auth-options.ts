c51f42b87eb26cf4acf7978206cb2c5203677fce
yukaorange <takaoka.webdesigner@gmail.com>
2024-10-15 00:26:30 +0900

    omit firebase

--- app/api/auth/[...nextauth]/auth-options.ts
+++ app/api/auth/[...nextauth]/auth-options.ts
@@ -1,6 +1,6 @@
-import { FirestoreAdapter } from '@next-auth/firebase-adapter';
+// import { FirestoreAdapter } from '@next-auth/firebase-adapter';
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
-import { cert } from 'firebase-admin/app';
+// import { cert } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import GoogleProvider from 'next-auth/providers/google';
@@ -71,14 +71,13 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
-  adapter: FirestoreAdapter({
-    credential: cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      // privateKey: process.env.FIREBASE_PRIVATE_KEY,
-    }),
-  }),
+  // adapter: FirestoreAdapter({
+  //   credential: cert({
+  //     projectId: process.env.FIREBASE_PROJECT_ID,
+  //     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  //     privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  //   }),
+  // }),
   callbacks: {
     async jwt({ token, user }) {
       console.log('JWT callback - user:', user);
@@ -108,5 +107,25 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: 'jwt',
     maxAge: 30 * 24 * 60 * 60,
+  },
+  events: {
+    async signIn(message) {
+      console.log('signIn', message);
+    },
+    async signOut(message) {
+      console.log('signOut', message);
+    },
+    async createUser(message) {
+      console.log('createUser', message);
+    },
+    async updateUser(message) {
+      console.log('updateUser', message);
+    },
+    async linkAccount(message) {
+      console.log('linkAccount', message);
+    },
+    async session(message) {
+      console.log('session', message);
+    },
   },
 };
