c3a429ebbd47b8b8c009921a238c53a04a8c7b96
yukaorange <takaoka.webdesigner@gmail.com>
2024-10-14 16:16:08 +0900

    modify config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,11 @@ const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, 'styles'), path.join(__dirname, 'components')],
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
   },
 }
 
